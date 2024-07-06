40480cc094926b50ddae05774d1f931f7b9d7983
Danila Kaslana <kallenkkaslana@gmail.com>
2024-07-06 17:39:05 +0300

    1

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,25 @@
 package com.epam.mjc.nio;
 
+import java.io.BufferedReader;
 import java.io.File;
-
+import java.io.IOException;
+import java.nio.*;
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        try(BufferedReader reader = new BufferedReader(new java.io.FileReader(file)))
+        {
+            String name = reader.readLine().replaceAll(".*: ", "");
+            String age = reader.readLine().replaceAll(".*: ", "");
+            String email = reader.readLine().replaceAll(".*: ", "");
+            String phone = reader.readLine().replaceAll(".*: ", "");
+            return  new Profile(name,Integer.valueOf(age),email,Long.valueOf(phone));
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+            return new Profile();
+        }
     }
 }
